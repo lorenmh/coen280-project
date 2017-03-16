af8dc8d438131ffbe9c9f4946039f86b97f5976a
betodt <alberto.diaz.tostado@gmail.com>
2017-03-15 17:30:35 -0700

    calculated probability of finding pairs for one user

--- scripts/spark/freq.py
+++ scripts/spark/freq.py
@@ -1,7 +1,7 @@
 #!/DCNFS/applications/cdh/5.6/app/spark-1.5.0-cdh5.6.0/bin/spark-submit
 
 from pyspark import SparkContext, SparkConf
-import itertools
+import itertools, math
 from ast import literal_eval as make_tuple
 
 conf = SparkConf()
@@ -18,7 +18,8 @@ data = data.map(lambda x: map(lambda s: str(s), x[1]))
 
 # print(data.take(5))
 
-support = data.count() * 0.005
+count = data.count()
+support = count * 0.002
 print(support)
 
 #transactions = data.map(lambda line: line.strip().split(' '))
@@ -30,9 +31,8 @@ transactions = data
 setSize = 1
 freq_item_set = {}
 
-while True:
+while setSize < 3:
 	new_fis = {}
-	print(setSize)
 	# model = FPGrowth.train(transactions, minSupport=0.2, numPartitions=10)
 	result = transactions.flatMap(lambda x: map(lambda y: (y, 1), itertools.combinations(x,setSize))).countByKey()
 	# result = model.freqItemsets().collect()
@@ -40,7 +40,6 @@ while True:
 	for fi in result:
 		if result[fi] >= support:
 			new_fis[fi] = result[fi]
-			print(fi, result[fi])
 
 	keys = [set(item) for item in new_fis]
 
@@ -50,10 +49,26 @@ while True:
 		# print(transactions.map(lambda t: list(set.union(*[x.intersection(set(t)) for x in keys]))).collect())
 		# Filter out non frequent sets
 		transactions = transactions.map(lambda t: list(set.union(*[x.intersection(set(t)) for x in keys])))
-		freq_item_set.update(new_fis)
+		freq_item_set.update({setSize: new_fis})
 
 	setSize += 1
 
-print(freq_item_set)
+setSize = 2
+sims = {}
+# generate association rules for sets larger than 1
+while setSize > 1:
+	for fis in freq_item_set[setSize]:
+		for item in itertools.combinations(fis, setSize-1):
+			if (item in freq_item_set[setSize-1]):
+				if (fis in sims):
+					sims[fis] *= freq_item_set[setSize][fis]/float(freq_item_set[setSize-1][item])
+				elif (fis[::-1] in sims):
+					sims[fis[::-1]] *= freq_item_set[setSize][fis]/float(freq_item_set[setSize-1][item])
+				else:
+					sims[fis] = freq_item_set[setSize][fis]/float(freq_item_set[setSize-1][item])
+	setSize -= 1
+
+for pair in sims:
+	print(pair, math.sqrt(sims[pair]))
 
 sc.stop()
